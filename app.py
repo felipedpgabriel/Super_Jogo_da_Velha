53c46cfdff194c727062ab8af164b7c1c3903de3
felipe.gabriel <felipe.gabriel@youxgroup.com.br>
2024-06-08 23:57:30 -0300

    feat: commit inicial.
    Inicio da lógica para testes com jogo da velha simples.

---
+++ app.py
@@ -0,0 +1,17 @@
+"""
+Script: app.py
+Author: Felipe de Paula Gabriel
+Date: 2024-06-08
+Version : 1.0
+Description: Script principal do Super Jogo da Velha.
+"""
+
+from elementos_de_jogo.tabuleiro import Tabuleiro
+
+
+if __name__ == "__main__":
+    # Alternar Jogador -> X e O
+    # Mostrar tabuleiro com as casas disponíveis
+    # Entrada da escolha
+    # Preencher campo com a escolha
+    tab = Tabuleiro()
